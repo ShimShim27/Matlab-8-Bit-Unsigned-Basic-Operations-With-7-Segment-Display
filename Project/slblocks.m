79ac182a2fb504da19bf49919973f7e53e6027f6
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2021-05-22 21:42:46 +0800

    Update

---
+++ Project/slblocks.m
@@ -0,0 +1,10 @@
+function blkStruct = slblocks
+
+    Browser.Library = 'custom_lib';
+   
+    Browser.Name = 'Custom Library';
+   
+    blkStruct.Browser = Browser;
+    
+    
+    %https://www.mathworks.com/help/simulink/ug/adding-libraries-to-the-library-browser.html
